1a478961167fe646370d7bc3f7086821991658e4
albertase <albertukaegbu@gmail.com>
2023-07-04 10:55:12 +0100

    added redirection linked 'RectLabs Inc' in the about page

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from "next/link";
 
 
 const About: React.FC = () => {
@@ -13,7 +14,7 @@ const About: React.FC = () => {
                         He is a skilled technical Entrepreneur with proven experience in AI
                         app conceptualization design, architectural flow, product
                         development, testing and building production level AI systems on
-                        GPUs and TPUs. He is currently building Rectlabs Inc, an AI based
+                        GPUs and TPUs. He is currently building <Link className="bg-[#585151] italic text-white border-b-4 hover:border-blue-700" href="https://rectlabs.com" as="https://rectlabs.com"> Rectlabs Inc,</Link> an AI based
                         research / production innovation company based in lagos, Nigeria. <br/><br />
 
                         He is a passionate individual, determined and result oriented engineer
