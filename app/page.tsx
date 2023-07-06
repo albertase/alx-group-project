7f052c6b0d8b713bee1263a653563435c3e118a5
albertase <albertukaegbu@gmail.com>
2023-07-06 22:46:55 +0100

    Added SEO

--- app/page.tsx
+++ app/page.tsx
@@ -7,58 +7,57 @@ import ProfileDetailed from "@/components/Profile/ProfileDetailed";
 import MyWork from "@/components/MyWork/MyWork";
 import Slide from "@/components/Slider/Slide";
 import Mentorship from "@/components/Mentorship/Mentorship";
-import Head from "next/head";
-import {Metadata} from "next";
-import {NextSeo} from "next-seo";
 import HeadComponent from "@/components/SeoComp/HeadComponent";
-
-
+import { NextSeo } from 'next-seo';
 
 export default function Home() {
     const pageTitle = "Landing Page";
-    const pageDescription = "This is Landing Page of Odemakinde Elisha";
+    const pageDescription = "This is the Landing Page of Odemakinde Elisha";
     const pageCanonical = "https://elishatofunmi.com/home";
-    const pageImage = "https://elishatofunmi.com/Elisha_bg.svg";
+    const pageImage = "https://elishatofunmi.com/contact_pic.png";
     const twitterUsername = "@elishatofunmi";
-  return (
-    <main className="">
-        <HeadComponent
-            title={pageTitle}
-            description={pageDescription}
-            canonical={pageCanonical}
-        />
-        <NextSeo
-            title={pageTitle}
-            description={pageDescription}
-            canonical={pageCanonical}
-            openGraph={{
-                url: pageCanonical,
-                title: pageTitle,
-                description: pageDescription,
-                images: [
-                    {
-                        url: pageImage,
-                        alt: pageTitle,
-                    },
-                ],
-                site_name: "Odemakinde Elisha Portfolio",
-            }}
-            twitter={{
-                handle: twitterUsername,
-                site: twitterUsername,
-                cardType: 'My background image',
-            }}
-        />
-      <Navbar />
-      <div className="mt-32">
-        <ProfileImage />
-        <ProfileDetailed/>
-        <MyWork/>
-        <Slide/>
-      </div>
-      <Mentorship/>
-      <NewsLetter/>
-      <Footer />
-    </main>
-  )
+
+    return (
+        <>
+            <NextSeo
+                title={pageTitle}
+                description={pageDescription}
+                canonical={pageCanonical}
+                openGraph={{
+                    url: pageCanonical,
+                    title: pageTitle,
+                    description: pageDescription,
+                    images: [
+                        {
+                            url: pageImage,
+                            alt: pageTitle,
+                        },
+                    ],
+                    site_name: "Odemakinde Elisha Portfolio",
+                }}
+                twitter={{
+                    handle: twitterUsername,
+                    site: twitterUsername,
+                    cardType: 'summary_large_image',
+                }}
+            />
+            <HeadComponent
+                title={pageTitle}
+                description={pageDescription}
+                canonical={pageCanonical}
+            />
+            <main className="">
+                <Navbar />
+                <div className="mt-32">
+                    <ProfileImage />
+                    <ProfileDetailed/>
+                    <MyWork/>
+                    <Slide/>
+                </div>
+                <Mentorship/>
+                <NewsLetter/>
+                <Footer />
+            </main>
+        </>
+    );
 }
